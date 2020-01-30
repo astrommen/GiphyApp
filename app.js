7d6817f1377a44b65df209fd8218d4f99a8418aa
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-01-30 10:34:07 -0500

    got new btns to show and work

--- app.js
+++ app.js
@@ -3,30 +3,51 @@ $(document).ready(function() {
     //var holding an array topic strings (1)
     var topics = ["bomber", "fighter-jet", "dirigible", "aerobatic"];
 
-    $.each(topics, function(index, value) {
+    
+    function loadBtns() {
         
-        // creating new buttons from topics array
-        var topicBtn = $("<button>");
+        $("#buttons-view").empty(); // stops buttons from stacking when submitting new
+        
+        $.each(topics, function(index, value) {
+            
+            // creating new buttons from topics array
+            var topicBtn = $("<button>");
+            
+            // adding a class of flight to our buttons
+            topicBtn.addClass("flight");
+            
+            // adding a data-attribute = value
+            topicBtn.attr("data-flight", value);
+            
+            // providing buttons with inner text
+            topicBtn.text(value);
+            
+            // appending buttons to container div
+            $("#buttons-view").append(topicBtn);     
+            
+        });
+    }
+    
+    // listens for submit button click
+    $(document).on("click", "#add-flight", function(){
 
-        // adding a class of flight to our buttons
-        topicBtn.addClass("flight");
+        event.preventDefault();
 
-        // adding a data-attribute = value
-        topicBtn.attr("data-flight", value);
+        
+        //pulls input string from form
+        var newFlite = $("#flight-input").val();
+    
+        topics.push(newFlite); // adds to topic array
 
-        // providing buttons with inner text
-        topicBtn.text(value);
+        loadBtns();
 
-        // appending buttons to container div
-        $("#buttons-view").append(topicBtn);     
-
-    });
+    })
 
     // code for GIPHY API call
     $(document).on("click", ".flight", function(){
 			
         // grabbing value from clicked button
-        var x = $(this).attr("data-flight");
+        var x = $(this).attr("data-flight").trim(); // .trim removes spaces & such
 
         // placing value into queryURL string w/ API key
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -68,4 +89,7 @@ $(document).ready(function() {
             }); // end of each loop
         }); // end of response function
     }); // end of document click event listener
+
+    loadBtns(); // initial load of buttons
+
 }); // end of document ready
