2e278613828b02fa02eb264ccd5e65b7e7822ae8
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-01-30 09:05:27 -0500

    added giphy api call + got rating to show

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// $(document).ready(function() {
+$(document).ready(function() {
     
     //var holding an array topic strings (1)
     var topics = ["bombers", "fighters", "dirigibles", "barnstormers"]; console.log(topics);
@@ -24,8 +24,39 @@
         $("#buttons-view").append(topicBtn);     
 
     });
-    // var p= $("<p>").text("Rating: " + results[i].rating);
+    // code for GIPHY API call
+    $(document).on("click", ".flight", function(){
+			
+        // grabbing value from clicked button
+        var x = $(this).attr("data-flight");
 
+        // placing value into queryURL string w/ API key
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            x + "&api_key=vRVtwSi9eBLLOpTHxJRJflgaFDvr7lH2&limit=10";
 
+        // API call
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function(response) { // complete response then exec function
+                
+            //empties the div so the new content doesnt stack
+            $("#gifs-appear-here").empty();
+        
+            // for loop running through data index
+            $.each(response.data, function(index, value){
 
-// })
+                var gifDiv = $("#gifs-appear-here"); // assign var to div for modularity
+
+                var gif = $("<div>"); // assign var to create new divs for gifs
+
+                // assign var to create new p for ratings
+                var p = $("<p>").text("Gif Rating: " + response.data[index].rating);
+            
+                gifDiv.append(p); // attach each p to the html div
+                gifDiv.append(gif); // attach new div to the html div
+
+            }); // end of each loop
+        }); // end of response function
+    }); // end of document click event listener
+}); // end of document ready
